236abc956d316d88c53c61346e718a29c865fc79
Deg42 <jaglezrivera@gmail.com>
2023-08-08 00:08:57 +0200

    "Initial Commit -lotofthings- 😌"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvestigatorComponent } from './investigator/investigator.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestigatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/investigator/investigator.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Investigator } from '../model/investigator.interface';
+
+@Component({
+  selector: 'app-investigator',
+  templateUrl: './investigator.component.html',
+  styleUrls: ['./investigator.component.css']
+})
+export class InvestigatorComponent implements OnInit {
+  investigators: Investigator[] = [];
+  selectedInvestigator: Investigator | undefined;
+  background: String = "";
+
+  ngOnInit(): void {
+    this.investigators = [
+      { name: "Roland Banks", image: "RolandBanks" },
+      { name: "Agnes Baker", image: "AgnesBaker" },
+      { name: "\"Malasombra\" O\'Toole", image: "SkidsOToole" },
+      { name: "Daisy Walker", image: "DaisyWalker" },
+      { name: "Wendy Adams", image: "WendyAdams" }
+    ]
+
+  }
+
+  onchange(): void {
+    this.background = "url(../../assets/img/investigators/" + this.selectedInvestigator!.image + "Token.png) no-repeat";
+  }
+
+}
