ee2dc0c6f2d9ecedf4cc2991113c7d241997d5af
Deg42 <jaglezrivera@gmail.com>
2023-08-09 00:52:04 +0200

    Added custom theme 🌈

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { InvestigatorComponent } from './investigator/investigator.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { CounterComponent } from './counter/counter.component';
+import { CaosComponent } from './caos/caos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvestigatorComponent
+    InvestigatorComponent,
+    CounterComponent,
+    CaosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     MatSelectModule
   ],
   providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }],
