dca52a57f35a097ba2f51b7a09d1c369d143e1d0
Deg42 <jaglezrivera@gmail.com>
2023-08-21 01:22:53 +0200

    Chaos bag animated and working 🏃‍♂️❓

--- src/app/caos/caos.component.ts
+++ src/app/caos/caos.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-caos',
   templateUrl: './caos.component.html',
-  styleUrls: ['./caos.component.css']
+  styleUrls: ['./caos.component.css'],
+  animations: [
+    trigger('flipping', [
+      state('before', style({
+        backgroundImage: 'url("../../assets/img/chaosBag/Chaos{{tokenImageBefore}}.png")',
+        transform: 'rotateY(0)'
+      }), { params: { tokenImageBefore: ' ' } }),
+      state('after', style({
+        backgroundImage: 'url("../../assets/img/chaosBag/Chaos{{tokenImageAfter}}.png")',
+        transform: 'rotateY(360deg)'
+      }), { params: { tokenImageAfter: ' ' } }),
+      transition('before => after', animate(500)),
+      transition('after => before', animate(500))
+    ])
+  ]
 })
-export class CaosComponent implements OnInit {
+export class CaosComponent {
 
-  difficulties: string[] = []
+  difficulties: string[] = ["Fácil", "Normal", "Difícil", "Experto"]
   selectedDifficulty: string = ""
-  chaosBag: string[] = [];
+  chaosBag: string[] = ["Plus1", "Plus1", "0", "0", "0", "Minus1", "Minus1", "Minus1", "Minus2", "Minus2", "Skull", "Skull", "Cultist", "Tablet", "Autofail", "ElderSign"];
+  history: string[] = ["", "", ""];
 
-  ngOnInit(): void {
-    this.difficulties = ["Fácil", "Normal", "Difícil", "Experto"];
-  }
+  state = 'before';
+  before = 'Minus1';
+  after = 'Cultist';
 
   onchange(): void {
     this.chaosBag = [];
@@ -31,8 +47,21 @@ export class CaosComponent implements OnInit {
         this.chaosBag.push("Plus1", "Plus1", "0", "0", "0", "Minus1", "Minus1", "Minus1", "Minus2", "Minus2", "Skull", "Skull", "Cultist", "Tablet", "Autofail", "ElderSign");
         break
       default:
+        this.chaosBag.push("Plus1", "Plus1", "0", "0", "0", "Minus1", "Minus1", "Minus1", "Minus2", "Minus2", "Skull", "Skull", "Cultist", "Tablet", "Autofail", "ElderSign");
         break;
-
     }
+  }
+
+  pullOutToken() {
+    if (this.state == 'before') {
+      this.after = this.chaosBag[Math.floor(Math.random() * this.chaosBag.length)];
+      this.state = 'after';
+      this.history.push(this.before);
+    } else if (this.state = 'after') {
+      this.before = this.chaosBag[Math.floor(Math.random() * this.chaosBag.length)];
+      this.state = 'before';
+      this.history.push(this.after);
+    }
+    this.history.length > 3 ? this.history.shift() : '';
   }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
 
-  counters: number[] = [0,0,0];
+  counters: number[] = [0, 2, 5];
 
   addCounter(index: number) {
     this.counters[index] >= 99 ? this.counters[index] : this.counters[index]++;
@@ -15,6 +15,18 @@ export class CounterComponent {
 
   substractCounter(index: number) {
     this.counters[index] <= 0 ? this.counters[index] : this.counters[index]--;
+  }
+
+  applyStyle(index: number) {
+    if (index === 0) {
+      return { color: 'DarkRed',
+      textShadow: '2px 0 var(--parchment), -2px 0 var(--parchment), 0 2px var(--parchment), 0 -2px var(--parchment), 1px 1px var(--parchment), -1px -1px var(--parchment), 1px -1px var(--parchment), -1px 1px var(--parchment)'
+     };
+    } else if (index <= 2) {
+      return { color: ' yellow' }
+    } else {
+      return { color: 'white' };
+    }
   }
 
 }
